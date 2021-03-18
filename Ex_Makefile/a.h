868d6e9edc99e429f1418b80d684e8bf6b172c33
biggiepants <biggiepants@korea.ac.kr>
2021-03-19 04:45:29 +0900

    Makefile Exercise

---
+++ Ex_Makefile/a.h
@@ -0,0 +1,6 @@
+#ifndef A_H  //중복으로 라이브러리를 호출하는것을 방지
+#define A_H
+
+void hello();
+
+#endif
---
+++ Ex_Makefile/main.cpp
@@ -0,0 +1,9 @@
+#include "a.h"  //C나 C++에서 기본적으로 제공하는 라이브러리는 <>
+#include "b.h"  //개인적으로 작성한 라이브러리 호출할땐 ""
+
+int main(){
+    
+    hello();
+    bye();
+    return 0;
+}
